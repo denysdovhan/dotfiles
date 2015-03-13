31eca86943c0255800abfbccaaf7d22c4398a418
Denys Dovhan <email@denysdovhan.com>
2015-03-13 23:25:28 +0200

    Added sync.py

---
+++ sync.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""
+Dotfiles syncronization.
+Makes symlinks for all files: ./bashrc will by available as ~/.bashrc.
+Source: https://gist.github.com/490016
+"""
+
+import os
+import glob
+
+EXCLUDE = ['sync.py']
+NO_DOT_PREFIX = []
+
+def forse_remove(path):
+    if os.path.isdir(path) and not os.path.islink(path):
+        shutil.rmtree(path, False, on_error)
+    else:
+        os.unlink(path)
+
+def is_link_to(link, dest):
+    is_link = os.path.islink(link)
+    is_link = is_link and os.readlink(link).rstrip('/') == dest.rstrip('/')
+    return is_link
+
+def main():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    for filename in [file for file in glob.glob('*') if file not in EXCLUDE]:
+        dotfile = filename
+        if filename not in NO_DOT_PREFIX:
+            dotfile = '.' + dotfile
+        dotfile = os.path.join(os.path.expanduser('~'), dotfile)
+        source = os.path.relpath(filename, os.path.dirname(dotfile))
+
+        # Check that we aren't overwriting anything
+        if os.path.exist(dotfile):
+            if is_link_to(dotfile, source):
+                continue
+
+            response = raw_input("Overwrite file `%s'? [y/N] " % dotfile)
+            if not response.lower().startwith('y'):
+                print "Skipping `%s'..." % dotfile
+                continue
+
+            forse_remove(dotfile)
+
+        os.symlink(source, dotfile)
+        print "%s => %s" % (dotfile, source)
+
+if __name__ == '__main__':
+    main()
