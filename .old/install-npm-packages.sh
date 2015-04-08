519f128f3ba183ed4e7832aca5e9d289f2b7ffc8
Denys Dovhan <email@denysdovhan.com>
2015-04-08 15:26:57 +0300

    Nodejs: Make nodejs installer better

--- .old/install-npm-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PACKAGES="less grunt-cli coffee-script bower nodemon\
-          express-generator hexo"
-
-# Installing global packages
-echo "Installing global packages..."
-npm i -g $PACKAGES && echo "Done!"
---
+++ setup/nodejs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Install node.js from nodesource.com
+echo "Installing nodejs..."
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs && echo "Done!"
+echo
+
+# Configure npm 
+# Ask required parameters
+echo "Configure npm init..."
+echo -n "What is your name?: " && read NAME
+echo -n "What is your email?: " && read EMAIL
+echo -n "What is your website?: " && read SITE
+
+# If required parameters dont entered, set them default values
+: ${NAME:="Denys Dovhan"}
+: ${EMAIL:="email@denysdovhan.com"}
+: ${SITE:="http://denysdovhan.com/"}
+
+echo "Author name put as: $NAME"
+npm set init.author.name "$NAME"
+echo "Author email put as: $EMAIL"
+npm set init.author.email "$EMAIL"
+echo "Author website put as: $SITE"
+npm set init.author.url "$SITE"
+echo
+
+# List of packages that must be installed globally
+PACKAGES="
+    less grunt-cli coffee-script bower nodemon express-generator hexo"
+
+# Installing global packages
+echo "Will be installed these packages:"
+echo $PACKAGES
+
+echo "Installing global packages..."
+sudo npm i -g $PACKAGES && echo "Done!"
