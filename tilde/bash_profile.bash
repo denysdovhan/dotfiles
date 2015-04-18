0ecfbed80cedf24ffc72ba60a8ca290a15e49b47
Denys Dovhan <email@denysdovhan.com>
2015-04-19 00:00:18 +0300

    Bash: Removed duplicates of fix 'only interactive'.

--- tilde/bash_profile.bash
+++ tilde/bash_profile.bash
@@ -5,9 +5,6 @@
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
 
-# If not running interactively, don't do anything
-[ -z "$PS1" ] && return
-
 # Prepend $PATH without duplicates
 function _prepend_path() {
  if ! $( echo "$PATH" | tr ":" "\n" | grep -qx "$1" ) ; then
