ae849fd3634327883de8e7816dda089afbd7fe42
Denys Dovhan <email@denysdovhan.com>
2015-06-19 01:16:05 +0300

    Setup: Added wifi-password and stylus to nodejs.sh.

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -32,7 +32,7 @@ echo
 # List of packages that must be installed globally
 PACKAGES="
     less grunt-cli coffee-script bower nodemon express-generator hexo yo
-    generator-email"
+    generator-email stylus nib wifi-password"
 
 # Installing global packages
 echo "Will be installed these packages:"
