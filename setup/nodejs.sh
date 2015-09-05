53929aa9b1c27b033c54d855157ba500ff4b6aa1
Denys Dovhan <email@denysdovhan.com>
2015-09-05 13:33:34 +0300

    Node: Add webpack, eslint.

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -32,7 +32,7 @@ echo
 # List of packages that must be installed globally
 PACKAGES="
     less grunt-cli coffee-script bower nodemon express-generator yo
-    stylus nib wifi-password gulp stylint"
+    stylus nib gulp stylint webpack eslint"
 
 # Installing global packages
 echo "Will be installed these packages:"
