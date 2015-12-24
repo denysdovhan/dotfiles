a82e6fd26607505eeb640112c6292ffe706060df
Denys Dovhan <email@denysdovhan.com>
2015-12-24 23:36:13 +0200

    Sync: Add backups.

--- sync.py
+++ sync.py
@@ -9,40 +9,62 @@ Source: https://github.com/sapegin/dotfiles/blob/master/sync.py
 
 import os
 import glob
+import shutil
 
-SOURCE_DIR = 'tilde'
-EXCLUDE = []
+DOTFILES_DIR  = os.path.dirname(os.path.abspath(__file__))
+SOURCE_DIR    = os.path.join(DOTFILES_DIR, 'tilde')
+BACKUP_DIR    = os.path.join(DOTFILES_DIR, 'backup')
+HOME_DIR      = os.path.expanduser('~')
+EXCLUDE       = []
 NO_DOT_PREFIX = []
 
+# remove path
 def forse_remove(path):
     if os.path.isdir(path) and not os.path.islink(path):
-        shutil.rmtree(path, False, on_error)
+        shutil.rmtree(path, False)
     else:
         os.unlink(path)
 
+# ckeck if is this a link to dest
 def is_link_to(link, dest):
     is_link = os.path.islink(link)
     is_link = is_link and os.readlink(link).rstrip('/') == dest.rstrip('/')
     return is_link
 
+# copy path to dest
+def copy(path, dest):
+    if os.path.isdir(path):
+        shutil.copytree(path, dest)
+    else:
+        shutil.copy(path, dest)
+
 def main():
-    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), SOURCE_DIR))
+    os.chdir(SOURCE_DIR)
     for filename in [file for file in glob.glob('*') if file not in EXCLUDE]:
         dotfile = filename
         if filename not in NO_DOT_PREFIX:
             dotfile = '.' + dotfile
-        dotfile = os.path.join(os.path.expanduser('~'), os.path.splitext(dotfile)[0])
+        dotfile = os.path.join(HOME_DIR, os.path.splitext(dotfile)[0])
         source = os.path.relpath(filename, os.path.dirname(dotfile))
 
-        # Check that we aren't overwriting anything
+        # check that we aren't overwriting anything
         if os.path.exists(dotfile):
             if is_link_to(dotfile, source):
                 continue
 
-            response = raw_input("Overwrite file `%s'? [y/N] " % dotfile)
-            if not response.lower().startswith('y'):
+            res = raw_input("Overwrite file `%s'? [y/N] " % dotfile)
+            if not res.lower().startswith('y'):
                 print "Skipping `%s'..." % dotfile
                 continue
+            else:
+                # Made backup copy if we're overwriting this file
+                res = raw_input("Make a backup of '%s'? [y/N] " % dotfile)
+                if res.lower().startswith('y'):
+                    if not os.path.exists(BACKUP_DIR):
+                        os.mkdir(BACKUP_DIR)
+                    backup = os.path.join(BACKUP_DIR, os.path.basename(dotfile))
+                    copy(dotfile, backup)
+                    print "Made a backup '%s'" % backup
 
             forse_remove(dotfile)
 
