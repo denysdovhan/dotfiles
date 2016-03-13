d18f1260e589ef4843029ee0aac29b1e734e883d
Denys Dovhan <email@denysdovhan.com>
2016-03-13 20:43:14 +0200

    Move ~/.zshlocal before zgen.

--- tilde/zshrc.zsh
+++ tilde/zshrc.zsh
@@ -105,6 +105,9 @@ HIST_STAMPS="dd.mm.yyyy"
 # Custom folder than $ZSH/custom
 ZSH_CUSTOM="$DOTFILES/zsh"
 
+# Load extra (private) settings
+[ -f ~/.zshlocal ] && source ~/.zshlocal
+
 # ZGEN
 # ====
 
@@ -145,6 +148,3 @@ if ! zgen saved; then
   # save all to init script
   zgen save
 fi
-
-# Load extra (private) settings
-[ -f ~/.zshlocal ] && source ~/.zshlocal
