e13cbda0ff05e32b0ab81c82040fb12cac92f9eb
Denys Dovhan <email@denysdovhan.com>
2016-04-24 01:01:41 +0300

    Zsh: Fix complete:13: command not found: compdef.

--- tilde/zshrc.zsh
+++ tilde/zshrc.zsh
@@ -34,7 +34,6 @@ fi
 if [ -d $HOME/.nvm ]; then
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-  [ -r "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
 fi
 
 # Default pager
