1f56041841b858fd10d3e42c4891b5072757881e
Denys Dovhan <denysdovhan@gmail.com>
2018-09-18 17:37:41 +0300

    Rewrite scripts/nodejs.zsh with new functions

--- scripts/nodejs.zsh
+++ scripts/nodejs.zsh
@@ -1,50 +1,125 @@
 #!/usr/bin/env zsh
 
-# Function for pretty headers
-function header() {
-  echo
-  echo -e "$(tput sgr 0 1)$(tput setaf 6)$1$(tput sgr0)"
+set -e
+trap on_error SIGKILL SIGTERM
+
+e='\033'
+RESET="${e}[0m"
+BOLD="${e}[1m"
+CYAN="${e}[0;96m"
+RED="${e}[0;91m"
+YELLOW="${e}[0;93m"
+GREEN="${e}[0;92m"
+
+# Success reporter
+info() {
+  echo -e "${CYAN}${*}${RESET}"
 }
 
-# Install Node.js
-header "Installing Node.js..."
-if [[ "$(uname)" == 'Darwin' ]]; then
-  brew install node
-else
-  curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-  sudo apt-get install -y nodejs
-fi
+ask() { 
+  printf "${YELLOW}${*}${RESET}"
+}
 
-# Install nvm
-header "Installing nvm..."
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+# Error reporter
+error() {
+  echo -e "${RED}${*}${RESET}"
+}
 
-# Configure npm
-# Ask required parameters
-header "Configure npm init..."
-echo "What is your name? (Denys Dovhan): " && read NAME
-echo "What is your email? (email@denysdovhan.com): " && read EMAIL
-echo "What is your website? (http://denysdovhan.com/): " && read SITE
+# Success reporter
+success() {
+  echo -e "${GREEN}${*}${RESET}"
+}
 
-# If required parameters dont entered, set them default values
-: ${NAME:="Denys Dovhan"}
-: ${EMAIL:="email@denysdovhan.com"}
-: ${SITE:="http://denysdovhan.com/"}
+# End section
+finish() {
+  success "Done!"
+  echo
+  sleep 1
+}
 
-echo "Author name set as: $NAME"
-npm set init.author.name "$NAME"
-echo "Author email set as: $EMAIL"
-npm set init.author.email "$EMAIL"
-echo "Author website set as: $SITE"
-npm set init.author.url "$SITE"
-echo
+install_node() {
+  info "Installing Node.js..."
 
-# Fixing npm permissions
-header "Fixing npm permissions..."
-NPM_PREFIX="$(npm config get prefix)"
-if [[ "${NPM_PREFIX}" = '/usr/local' ]]; then
-  sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
-elif [ ! "$NPM_PREFIX" = '/usr' ]; then
-	[ ! -d ~/.npm-global ] && mkdir ~/.npm-global
-	npm config set prefix '~/.npm-global'
-fi
+  if [[ "$(uname)" == 'Darwin' ]]; then
+    brew install node
+  else
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get install -y nodejs build-essential
+  fi
+
+  finish
+}
+
+install_nvm() {
+  # Install nvm
+  info "Installing nvm..."
+
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+  finish
+}
+
+configure_npm_init() {
+  # Ask required parameters
+  info "Configure npm init..."
+
+  # Defaults
+  local name="Denys Dovhan"
+  local email="denysdovhan@gmail.com"
+  local website="http://denysdovhan.com/"
+
+  ask "What is your name? ($name): " && read EMAIL
+  ask "What is your email? ($email): " && read EMAIL
+  ask "What is your website? ($website): " && read SITE
+
+  # If required parameters are not entered, set them default values
+  : ${NAME:="$name"}
+  : ${EMAIL:="$email"}
+  : ${WEBSITE:="$website"}
+
+  echo "Author name set as: $NAME"
+  npm set init.author.name "$NAME"
+  echo "Author email set as: $EMAIL"
+  npm set init.author.email "$EMAIL"
+  echo "Author website set as: $WEBSITE"
+  npm set init.author.url "$WEBSITE"
+  echo
+
+  finish
+}
+
+fix_npm_perm() {
+  # Fixing npm permissions
+  info "Fixing npm permissions..."
+
+  NPM_PREFIX="$(npm config get prefix)"
+  if [[ "${NPM_PREFIX}" = '/usr/local' ]]; then
+    sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+  elif [ ! "$NPM_PREFIX" = '/usr' ]; then
+    [ ! -d ~/.npm-global ] && mkdir ~/.npm-global
+    npm config set prefix '~/.npm-global'
+  fi
+
+  finish
+}
+
+on_finish() {
+  success "Done!"
+  success "Node.js is installed!"
+  echo
+}
+
+on_error() {
+  error "Wow... Something serious happened!"
+  error "Though, I don't know what really happened :("
+  exit 1
+}
+
+main() {
+  install_node "$*"
+  install_nvm "$*"
+  configure_npm_init "$*"
+  fix_npm_perm "$*"
+}
+
+main "$*"
